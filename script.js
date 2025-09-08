76fb0650b49dd4faa0847962919f5ae1ece6719d
Omar Loutfy <oloutfy19@gmail.com>
2025-09-08 02:17:38 +0200

    Adjust script.js functionalities

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function Book(title, author, pages) {
+// Functional programming only
+
+function Book({title, author, pages}) {
     // Constructor that creates a book object
     this.title = title;
     this.author = author;
@@ -6,39 +8,56 @@ function Book(title, author, pages) {
     this.id = crypto.randomUUID()
 }
 
-function addBookToLibrary(title, author, pages) {
+function addBookToLibrary({title, author, pages}) {
     // Function that takes params, create a book then store it in the array
-    myLibrary.push(new Book(title, author, pages))
+    myLibrary.push(new Book({
+        title: title,
+        author: author, 
+        pages: pages
+    }))
 }
 
-function loopThroughBookArrayAndDisplayBooks() {
+function loopThroughBookArrayAndDisplayBooks(bookContainer) {
     // Function that creates a div for each book object and puts it in the DOM
-    myLibrary.forEach(element => {
-        const bookItemDiv = document.createElement("div");
-        const bookItemDivTitle = document.createElement("div");
-        const bookItemDivAuthor = document.createElement("div");
-        const bookItemDivPages = document.createElement("div");
+    myLibrary.forEach(book => {
 
-        bookItemDiv.className = "book-item";
-        bookItemDiv.setAttribute("data-id", element.id)
-        bookItemDivTitle.className = "book-item-title";
-        bookItemDivAuthor.className = "book-item-author";
-        bookItemDivPages.className = "book-item-pages";
+        const bookItemDiv = renderBook(book)
 
-        bookItemDivTitle.append(`${element.title}`);
-        bookItemDivAuthor.append(`Author: ${element.author}`);
-        bookItemDivPages.append(`Pages: ${element.pages}`);
+        const {
+            bookItemDiv: newBookItemDiv, 
+            bookItemButtonsDiv: newBookItemButtonsDiv
+        } = insertBookDivButtonsWithFunctionalities(bookItemDiv)
 
-        bookContainer.appendChild(bookItemDiv);
-        bookItemDiv.appendChild(bookItemDivTitle);
-        bookItemDiv.appendChild(bookItemDivAuthor);
-        bookItemDiv.appendChild(bookItemDivPages);
-        
-        insertBookDivButtonsWithFunctionalities(bookItemDiv)
+        addEventListenerToRemoveButton(newBookItemDiv);
+        addEventListenersToReadButton(newBookItemButtonsDiv);
     });
 }
 
-function insertBookDivButtonsWithFunctionalities(div) {
+function renderBook(book) {
+    const bookItemDiv = document.createElement("div");
+    const bookItemDivTitle = document.createElement("div");
+    const bookItemDivAuthor = document.createElement("div");
+    const bookItemDivPages = document.createElement("div");
+
+    bookItemDiv.className = "book-item";
+    bookItemDiv.setAttribute("data-id", book.id)
+    bookItemDivTitle.className = "book-item-title";
+    bookItemDivAuthor.className = "book-item-author";
+    bookItemDivPages.className = "book-item-pages";
+
+    bookItemDivTitle.textContent = `${book.title}`;
+    bookItemDivAuthor.textContent = `Author: ${book.author}`;
+    bookItemDivPages.textContent = `Pages: ${book.pages}`;
+
+    bookContainer.appendChild(bookItemDiv);
+    bookItemDiv.appendChild(bookItemDivTitle);
+    bookItemDiv.appendChild(bookItemDivAuthor);
+    bookItemDiv.appendChild(bookItemDivPages);
+    
+    return bookItemDiv;
+}
+
+function insertBookDivButtonsWithFunctionalities(bookItemDiv) {
     // Add "Read" and "Remove" buttons to argument div
     const bookItemButtonsDiv = document.createElement("div");
     const bookItemRemoveButton = document.createElement("button");
@@ -48,25 +67,22 @@ function insertBookDivButtonsWithFunctionalities(div) {
     bookItemRemoveButton.className = "book-item-remove-button";
     bookItemReadButton.className = "book-item-read-button";
 
-    bookItemRemoveButton.append("Remove");
-    bookItemReadButton.append("Read");
+    bookItemRemoveButton.textContent = "Remove";
+    bookItemReadButton.textContent = "Read";
     bookItemReadButton.style.backgroundColor = "rgba(201, 233, 201, 1)";
     bookItemReadButton.style.fontWeight = "bold";
     bookItemRemoveButton.style.fontWeight = "bold";
 
-    div.appendChild(bookItemButtonsDiv);
+    bookItemDiv.appendChild(bookItemButtonsDiv);
     bookItemButtonsDiv.appendChild(bookItemRemoveButton);
     bookItemButtonsDiv.appendChild(bookItemReadButton);
 
-    addEventListenersToReadButton(bookItemButtonsDiv);
+    return {bookItemDiv, bookItemButtonsDiv};
 }
 
-function addEventListenersToReadButton(div) {
-    // Add Event listener to "Read" and "Not Read" button
-    const removeButton = div.querySelector(".book-item-remove-button");
-    const readButton = div.querySelector(".book-item-read-button");
-
-    
+function addEventListenersToReadButton(buttonsDiv) {
+    // Add Event listener to the "Read" / "Not Read" button
+    const readButton = buttonsDiv.querySelector(".book-item-read-button");
     readButton.addEventListener("click", function(event) {
         if (event.target.textContent === "Read") {
             event.target.textContent = "Not read";
@@ -76,26 +92,61 @@ function addEventListenersToReadButton(div) {
             event.target.style.backgroundColor = "rgba(201, 233, 201, 1)";
         }
     })
-
-    removeButton.addEventListener("click", () => {
-        
-    })
-
-
-
 }
 
-// Array that will contain different book objects through addBookToLibrary function
-const myLibrary = [];   
-addBookToLibrary("The Hobbit", "J. R. R. Tolkien", "320");
-addBookToLibrary("Harry Potter and the Prisoner of Azkaban", "J. K. Rowling", "317 ");
-addBookToLibrary("Of Mice and Men", "John Steinbeck", "107");
+function addEventListenerToRemoveButton(bookItemDiv) {
+    // Add Event listener to the "Remove" button
+    const currentDivId = bookItemDiv.dataset.id
+    const removeButton = bookItemDiv.querySelector(".book-item-remove-button");
+    removeButton.addEventListener("click", () => {
+        myLibrary.splice(
+            myLibrary.findIndex(book => book.id === currentDivId),
+            1
+        )
+
+        bookItemDiv.remove()
+    })
+}
+
+// Array that will contain current book objects at all given times
+let myLibrary = [];   
+addBookToLibrary({
+    title: "The Hobbit", 
+    author: "J. R. R. Tolkien", 
+    pages: "320"
+});
+addBookToLibrary({
+    title: "Harry Potter and the Prisoner of Azkaban", 
+    author: "J. K. Rowling", 
+    pages: "317"
+});
+addBookToLibrary({
+    title: "Of Mice and Men", 
+    author: "John Steinbeck", 
+    pages: "107"
+});
+addBookToLibrary({
+    title: "Le Mage du Kremlin", 
+    author: "Roman de Giuliano da Empoli", 
+    pages: "320"
+});
+addBookToLibrary({
+    title: "Au sud de la frontière, à l'ouest du soleil", 
+    author: "Roman de Giuliano da Empoli", 
+    pages: "264"
+});
+addBookToLibrary({
+    title: "Introduction to Graph Theory", 
+    author: "Richard J Trudeau", 
+    pages: "222"
+});
 
 // Populate book container with objects found in myLibrary array
 const bookContainer = document.getElementById("book-container");
-loopThroughBookArrayAndDisplayBooks();
+loopThroughBookArrayAndDisplayBooks(bookContainer);
 
 // Add an EventListener to the add book button that dislpay the modal
+const dialog = document.querySelector("#add-book-button-dialog");
 const addBookButton = document.getElementById("add-book-button");
 const addBookButtonDialogue = document.getElementById("add-book-button-dialog");
 addBookButton.addEventListener("click", () => {
@@ -104,19 +155,20 @@ addBookButton.addEventListener("click", () => {
 
 // Take the form results and create a new book item
 const submitFormButton = document.getElementById("submit-form-button");
-submitFormButton.addEventListener("click", () => {
+submitFormButton.addEventListener("click", (event) => {
+    event.preventDefault();
     myLibrary.push(
-        new Book(
-            document.querySelector(".form-entry > #title").value,
-            document.querySelector(".form-entry > #author").value,
-            document.querySelector(".form-entry > #pages").value
-        )
+        new Book({
+            title: document.querySelector(".form-entry > #title").value,
+            author: document.querySelector(".form-entry > #author").value,
+            pages: document.querySelector(".form-entry > #pages").value
+        })
     )
 
-    newBookItem = document.createElement("div");
-    newBookItemTitle = document.createElement("div");
-    newBookItemAuthor = document.createElement("div");
-    newBookItemPages = document.createElement("div");
+    const newBookItem = document.createElement("div");
+    const newBookItemTitle = document.createElement("div");
+    const newBookItemAuthor = document.createElement("div");
+    const newBookItemPages = document.createElement("div");
     
     newBookItem.className = "book-item";
     newBookItem.setAttribute("data-id", myLibrary[myLibrary.length - 1].id);
@@ -124,28 +176,24 @@ submitFormButton.addEventListener("click", () => {
     newBookItemAuthor.className = "book-item-author";
     newBookItemPages.className = "book-item-pages";
 
-    newBookItemTitle.append(myLibrary[myLibrary.length - 1].title);
-    newBookItemAuthor.append(myLibrary[myLibrary.length - 1].author);
-    newBookItemPages.append(myLibrary[myLibrary.length - 1].pages);
+    const addedBook = myLibrary[myLibrary.length - 1];
+    newBookItemTitle.textContent = addedBook.title;
+    newBookItemAuthor.textContent = `Author: ${addedBook.author}`;
+    newBookItemPages.textContent = `Pages: ${addedBook.pages}`;
 
     bookContainer.appendChild(newBookItem);
     newBookItem.appendChild(newBookItemTitle);
     newBookItem.appendChild(newBookItemAuthor);
     newBookItem.appendChild(newBookItemPages);
 
-    const originalButtons = document.querySelector(".book-item-buttons");
-    const clonedButtons = originalButtons.cloneNode(true)
+    const {
+        bookItemDiv: newBookItemDiv, 
+        bookItemButtonsDiv: newBookItemButtonsDiv
+    } = insertBookDivButtonsWithFunctionalities(newBookItem)
 
-    newBookItem.appendChild(clonedButtons);
+    addEventListenerToRemoveButton(newBookItemDiv);
+    addEventListenersToReadButton(newBookItemButtonsDiv);
+
+    dialog.close()
+
 })
-
-// // Add remove functionalities to the remove buttons
-// const allRemoveItembuttonArray = document.querySelectorAll(".book-item-remove-button")
-// allRemoveItembuttonArray.forEach(element => {
-//     element.addEventListener("click", () => {
-//         element.parentElement.parentElement .remove()
-//     })
-// });
-
-
-
