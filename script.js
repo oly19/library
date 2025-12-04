d0755a868f2a722eab305e7727633017dca26589
Omar Loutfy <oloutfy19@gmail.com>
2025-12-04 22:27:30 +0100

    Restore last version of project

--- script.js
+++ script.js
@@ -1,199 +1,187 @@
-// Functional programming only
+const Gameboard = (function () {
+  const currentGameBoard = [
+    [null, null, null], 
+    [null, null, null], 
+    [null, null, null]
+  ];
 
-function Book({title, author, pages}) {
-    // Constructor that creates a book object
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.id = crypto.randomUUID()
-}
+  const trackTurnArray = [];
 
-function addBookToLibrary({title, author, pages}) {
-    // Function that takes params, create a book then store it in the array
-    myLibrary.push(new Book({
-        title: title,
-        author: author, 
-        pages: pages
-    }))
-}
+  const getScore = () => {
+    console.log("Current score board:")
+    console.log(currentGameBoard.map(row => [...row]));
+    return currentGameBoard
+  };
 
-function loopThroughBookArrayAndDisplayBooks(bookContainer) {
-    // Function that creates a div for each book object and puts it in the DOM
-    myLibrary.forEach(book => {
+  const pushPlayerTurnToArray = function (player) {
+    trackTurnArray.push(player.name);
+  }
+  
+  const trackTurnOrder = function (player) {
+    console.log(trackTurnArray)
+  }
 
-        const bookItemDiv = renderBook(book)
+  const validateTurnOrder = function (player) {
+    const turnLen = trackTurnArray.length
+    if (trackTurnArray[turnLen - 1] === player.name) {
+    console.log(
+      `Invalid input. On turn ${turnLen + 1}, ${player.name} ` +
+      `tried to play a second turn in a row!`
+    );
+      return true;
+    }
+  }
 
-        const {
-            bookItemDiv: newBookItemDiv, 
-            bookItemButtonsDiv: newBookItemButtonsDiv
-        } = insertBookDivButtonsWithFunctionalities(bookItemDiv)
-
-        addEventListenerToRemoveButton(newBookItemDiv);
-        addEventListenersToReadButton(newBookItemButtonsDiv);
-    });
-}
-
-function renderBook(book) {
-    const bookItemDiv = document.createElement("div");
-    const bookItemDivTitle = document.createElement("div");
-    const bookItemDivAuthor = document.createElement("div");
-    const bookItemDivPages = document.createElement("div");
-
-    bookItemDiv.className = "book-item";
-    bookItemDiv.setAttribute("data-id", book.id)
-    bookItemDivTitle.className = "book-item-title";
-    bookItemDivAuthor.className = "book-item-author";
-    bookItemDivPages.className = "book-item-pages";
-
-    bookItemDivTitle.textContent = `${book.title}`;
-    bookItemDivAuthor.textContent = `Author: ${book.author}`;
-    bookItemDivPages.textContent = `Pages: ${book.pages}`;
-
-    bookContainer.appendChild(bookItemDiv);
-    bookItemDiv.appendChild(bookItemDivTitle);
-    bookItemDiv.appendChild(bookItemDivAuthor);
-    bookItemDiv.appendChild(bookItemDivPages);
+  const validateInRangeBoardInput = function(player, row, column) {
+    if (row > 2 || column > 2) {
+      console.log(
+        `Invalid input. On turn ${trackTurnArray.length + 1}, ${player.name} ` + 
+        `tried to input the "${player.sign}" sign in cell ` +
+        `(${row + 1},${column + 1}) which is out of range!`
+      )
+      return true
+    }
+  }
     
-    return bookItemDiv;
-}
-
-function insertBookDivButtonsWithFunctionalities(bookItemDiv) {
-    // Add "Read" and "Remove" buttons to argument div
-    const bookItemButtonsDiv = document.createElement("div");
-    const bookItemRemoveButton = document.createElement("button");
-    const bookItemReadButton = document.createElement("button");
-
-    bookItemButtonsDiv.className = "book-item-buttons"
-    bookItemRemoveButton.className = "book-item-remove-button";
-    bookItemReadButton.className = "book-item-read-button";
-
-    bookItemRemoveButton.textContent = "Remove";
-    bookItemReadButton.textContent = "Read";
-    bookItemReadButton.style.backgroundColor = "rgba(201, 233, 201, 1)";
-    bookItemReadButton.style.fontWeight = "bold";
-    bookItemRemoveButton.style.fontWeight = "bold";
-
-    bookItemDiv.appendChild(bookItemButtonsDiv);
-    bookItemButtonsDiv.appendChild(bookItemRemoveButton);
-    bookItemButtonsDiv.appendChild(bookItemReadButton);
-
-    return {bookItemDiv, bookItemButtonsDiv};
-}
-
-function addEventListenersToReadButton(buttonsDiv) {
-    // Add Event listener to the "Read" / "Not Read" button
-    const readButton = buttonsDiv.querySelector(".book-item-read-button");
-    readButton.addEventListener("click", function(event) {
-        if (event.target.textContent === "Read") {
-            event.target.textContent = "Not read";
-            event.target.style.backgroundColor = "rgb(255, 186, 186)";
-        } else {
-            event.target.textContent = "Read";
-            event.target.style.backgroundColor = "rgba(201, 233, 201, 1)";
-        }
-    })
-}
-
-function addEventListenerToRemoveButton(bookItemDiv) {
-    // Add Event listener to the "Remove" button
-    const currentDivId = bookItemDiv.dataset.id
-    const removeButton = bookItemDiv.querySelector(".book-item-remove-button");
-    removeButton.addEventListener("click", () => {
-        myLibrary.splice(
-            myLibrary.findIndex(book => book.id === currentDivId),
-            1
+  const validateEmptyBoardInput = function (player, row, column) {
+      if (currentGameBoard[row][column] !== null) {
+        console.log(
+          `Invalid input. On turn ${trackTurnArray.length + 1}, ${player.name} ` + 
+          `tried to input the "${player.sign}" sign in a non empty cell!`
         )
+        return true
+    }
+  }
 
-        bookItemDiv.remove()
-    })
+  const checkIfGameIsWon = function (player) {
+    if (trackTurnArray.length < 5) {
+      return
+    }
+
+    const winConfigurationArray = [
+      [currentGameBoard[0][0], currentGameBoard[0][1], currentGameBoard[0][2]],   // First line 
+      [currentGameBoard[1][0], currentGameBoard[1][1], currentGameBoard[1][2]],   // Second line
+      [currentGameBoard[2][0], currentGameBoard[2][1], currentGameBoard[2][2]],   // Third line
+      [currentGameBoard[0][0], currentGameBoard[1][0], currentGameBoard[2][0]],   // First column
+      [currentGameBoard[0][1], currentGameBoard[1][1], currentGameBoard[2][1]],   // Second column
+      [currentGameBoard[0][2], currentGameBoard[1][2], currentGameBoard[2][2]],   // Third column
+      [currentGameBoard[0][0], currentGameBoard[1][1], currentGameBoard[2][2]],   // First diagonal
+      [currentGameBoard[0][2], currentGameBoard[1][1], currentGameBoard[2][0]],   // Second diagonal
+    ];
+
+    const winArray = winConfigurationArray.filter(
+      winConfig => winConfig.every(cell => cell === winConfig[0])
+    );
+
+    if (winArray) {
+      console.log(`Game is over, ${player.name} has won!`)
+      return true
+    }
+  }
+
+  const checkIfGameIsTied = function () {
+    if (trackTurnArray.length === 9) {
+      console.log(`Game is over, it's a tie!`)
+      return true
+    }
+  }
+
+  const playTurn = function (player, row, column) {
+    if (validateTurnOrder(player)) {
+      return;
+    }
+    if (validateInRangeBoardInput(player, row, column)) {
+      return;
+    }
+    if (validateEmptyBoardInput(player, row, column)) {
+      return;
+    }
+
+    pushPlayerTurnToArray(player);
+    trackTurnOrder();
+    currentGameBoard[row][column] = player.sign
+    console.log(
+      `On turn ${trackTurnArray.length}, ${player.name} placed the "${player.sign}" symbol ` +
+      `in row ${row + 1} and column ${column + 1}.`
+    );
+
+    getScore()
+
+    if (checkIfGameIsWon(player)) {
+      return
+    }
+    checkIfGameIsTied()
+  };
+
+  return { getScore, playTurn, trackTurnOrder};
+
+})();
+
+
+function createPlayer(name, sign) {
+  return { name, sign };
 }
 
-// Array that will contain current book objects at all given times
-let myLibrary = [];   
-addBookToLibrary({
-    title: "The Hobbit", 
-    author: "J. R. R. Tolkien", 
-    pages: "320"
-});
-addBookToLibrary({
-    title: "Harry Potter and the Prisoner of Azkaban", 
-    author: "J. K. Rowling", 
-    pages: "317"
-});
-addBookToLibrary({
-    title: "Of Mice and Men", 
-    author: "John Steinbeck", 
-    pages: "107"
-});
-addBookToLibrary({
-    title: "Le Mage du Kremlin", 
-    author: "Roman de Giuliano da Empoli", 
-    pages: "320"
-});
-addBookToLibrary({
-    title: "Au sud de la frontière, à l'ouest du soleil", 
-    author: "Roman de Giuliano da Empoli", 
-    pages: "264"
-});
-addBookToLibrary({
-    title: "Introduction to Graph Theory", 
-    author: "Richard J Trudeau", 
-    pages: "222"
-});
 
-// Populate book container with objects found in myLibrary array
-const bookContainer = document.getElementById("book-container");
-loopThroughBookArrayAndDisplayBooks(bookContainer);
 
-// Add an EventListener to the add book button that dislpay the modal
-const dialog = document.querySelector("#add-book-button-dialog");
-const addBookButton = document.getElementById("add-book-button");
-const addBookButtonDialogue = document.getElementById("add-book-button-dialog");
-addBookButton.addEventListener("click", () => {
-    addBookButtonDialogue.showModal()
-});
 
-// Take the form results and create a new book item
-const submitFormButton = document.getElementById("submit-form-button");
-submitFormButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    myLibrary.push(
-        new Book({
-            title: document.querySelector(".form-entry > #title").value,
-            author: document.querySelector(".form-entry > #author").value,
-            pages: document.querySelector(".form-entry > #pages").value
-        })
-    )
+// DOM Manipulation //
 
-    const newBookItem = document.createElement("div");
-    const newBookItemTitle = document.createElement("div");
-    const newBookItemAuthor = document.createElement("div");
-    const newBookItemPages = document.createElement("div");
-    
-    newBookItem.className = "book-item";
-    newBookItem.setAttribute("data-id", myLibrary[myLibrary.length - 1].id);
-    newBookItemTitle.className = "book-item-title";
-    newBookItemAuthor.className = "book-item-author";
-    newBookItemPages.className = "book-item-pages";
+const mainContainer = document.querySelector("#main-container");
+const startGameButton = document.querySelector("#start-game-button");
 
-    const addedBook = myLibrary[myLibrary.length - 1];
-    newBookItemTitle.textContent = addedBook.title;
-    newBookItemAuthor.textContent = `Author: ${addedBook.author}`;
-    newBookItemPages.textContent = `Pages: ${addedBook.pages}`;
+startGameButton.addEventListener("click", () => {
+  startGameButton.remove()
 
-    bookContainer.appendChild(newBookItem);
-    newBookItem.appendChild(newBookItemTitle);
-    newBookItem.appendChild(newBookItemAuthor);
-    newBookItem.appendChild(newBookItemPages);
-
-    const {
-        bookItemDiv: newBookItemDiv, 
-        bookItemButtonsDiv: newBookItemButtonsDiv
-    } = insertBookDivButtonsWithFunctionalities(newBookItem)
-
-    addEventListenerToRemoveButton(newBookItemDiv);
-    addEventListenersToReadButton(newBookItemButtonsDiv);
-
-    dialog.close()
-
+  const startGameButtonDialogue = document.getElementById("input-players-names-dialogue");
+  startGameButtonDialogue.showModal()
 })
+
+
+const submitPlayersInput = document.getElementById("submit-form-button");
+submitPlayersInput.addEventListener("click", () => {
+  const gameBoardContainer = document.createElement("div");
+  gameBoardContainer.id = "game-board-container";
+  gameBoardContainer.style.display = "grid";
+  gameBoardContainer.style.gridTemplateColumns = "repeat(3, minmax(100px, 150px))";
+  gameBoardContainer.style.gridTemplateRows = "repeat(3, minmax(100px, 150px))";
+  mainContainer.appendChild(gameBoardContainer)
+
+  for (let i = 0; i < 9; i++) {
+    const gameBoardCell = document.createElement("div");
+    gameBoardCell.className = "game-board-cell";
+    
+    gameBoardCell.setAttribute("data-row", Math.floor(i / 3) + 1);
+    gameBoardCell.setAttribute("data-column", (i % 3) + 1);
+
+    gameBoardCell.style.border = "solid 1px black";
+
+    gameBoardContainer.appendChild(gameBoardCell);
+  }
+
+  gameBoardContainer.querySelectorAll(".game-board-cell[data-row='1']").forEach((element) => {
+    element.style["border-top"] = "none"
+  })
+  gameBoardContainer.querySelectorAll(".game-board-cell[data-row='3']").forEach((element) => {
+    element.style["border-bottom"] = "none"
+  })
+  gameBoardContainer.querySelectorAll(".game-board-cell[data-column='1']").forEach((element) => {
+    element.style["border-left"] = "none"
+  })  
+  gameBoardContainer.querySelectorAll(".game-board-cell[data-column='3']").forEach((element) => {
+    element.style["border-right"] = "none"
+  })
+})
+
+
+const player1 = createPlayer("Omarrrr", "X");
+const player2 = createPlayer("Ali", "O");
+
+
+
+// const playerOneContainer = document.querySelector('.player-container[data-player="1"]');
+// playerOneContainer.textContent = `${player1.name} (${player1.sign})`
+
+// const playerTwoContainer = document.querySelector('.player-container[data-player="2"]');
+// playerTwoContainer.textContent = `${player2.name} (${player2.sign})`
+
